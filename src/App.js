8486e78c3ac088db34189b4abfe61cb801f0c7d5
Abhishek Chaturvedi <abhishek.chaturvedi@cognizant.com>
2021-04-25 23:37:18 +0530

    redux-persist

--- src/App.js
+++ src/App.js
@@ -3,25 +3,33 @@ import Navbar from "./components/coreComponents/navbar/Navbar";
 import { Provider } from "react-redux";
 import "./styles/App.scss";
 import store from "./redux/store";
+import persistor from "./redux/store";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddContact from "./components/addContact/AddContact";
 import EditContact from "./components/editContact/EditContact";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Contacts} />
-              <Route exact path="/contacts/add" component={AddContact} />
-              <Route exact path="/contacts/edit/:id" component={EditContact} />
-            </Switch>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contacts/add" component={AddContact} />
+                <Route
+                  exact
+                  path="/contacts/edit/:id"
+                  component={EditContact}
+                />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,24 @@
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-// import contactsReducer from "./reducer/contactsReducer/ContactsReducer";
+// storing data in local storage
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 import rootReducer from "./reducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(rootReducer, composeWithDevTools());
 
-export default store;
+export default () => {
+  let store = createStore(persistedReducer);
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
+
+// export default store;
